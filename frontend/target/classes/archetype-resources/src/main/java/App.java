c583f3e498d51a1fb3598be27a3adf49b4e282fe
Alexis Frantz <loyallombardi@gmail.com>
2023-03-02 16:52:13 +0100

    plugins configured

---
+++ frontend/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.alexisfrantz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
