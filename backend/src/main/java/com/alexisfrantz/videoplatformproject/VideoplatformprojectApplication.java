c38470c5eff732750ddf98ad7e2865c817f3e32a
Alexis Frantz <loyallombardi@gmail.com>
2023-03-02 16:39:47 +0100

    Initial Setup, Modules

---
+++ backend/src/main/java/com/alexisfrantz/videoplatformproject/VideoplatformprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alexisfrantz.videoplatformproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoplatformprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoplatformprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alexisfrantz/videoplatformproject/VideoplatformprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexisfrantz.videoplatformproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoplatformprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
